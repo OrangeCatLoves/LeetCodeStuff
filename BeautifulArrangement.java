67f165a8f7fba6da1d9c9cf7add87549a0ad4530
OrangeCatLoves <e1121707@u.nus.edu>
2024-07-29 20:00:19 +0800

    Add files via upload

---
+++ BeautifulArrangement.java
@@ -0,0 +1,26 @@
+class Solution {
+    private int result;
+    public int countArrangement(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        boolean[] isUsed = new boolean[n + 1];
+        Helper(n, 1, isUsed);
+        return this.result;
+    }
+
+    public void Helper(int n, int index, boolean[] isUsed) {
+        if (index == n + 1) {
+            System.out.println("A");
+            this.result++;
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!isUsed[i] && (i % index == 0 || index % i == 0)) {
+                isUsed[i] = true;
+                Helper(n, index + 1, isUsed);
+                isUsed[i] = false;
+            }
+        }
+    }
+}
