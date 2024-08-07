2d2ed768b98a0501748183749e66bdea1fa24f5c
Low Wei Bin <e1121707@u.nus.edu>
2024-08-07 16:32:02 +0800

    Update MyCalendarI.java

--- MyCalendarI.java
+++ MyCalendarI.java
@@ -8,6 +8,9 @@ class MyCalendar {
 
     public boolean book(int start, int end) {
         for (int[] interval : this.Calender) {
+            if (interval[0] > end) {
+                break;
+            }
             if (interval[1] > start && end > interval[0]) {
                 return false;
             }
