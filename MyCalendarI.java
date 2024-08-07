4787fae07b1b65e7d06c2dfb1a9b6805af2b08ec
Low Wei Bin <e1121707@u.nus.edu>
2024-08-07 16:28:50 +0800

    Add files via upload

---
+++ MyCalendarI.java
@@ -0,0 +1,26 @@
+class MyCalendar {
+
+    private List<int[]> Calender;
+
+    public MyCalendar() {
+        this.Calender = new ArrayList<>();
+    }
+
+    public boolean book(int start, int end) {
+        for (int[] interval : this.Calender) {
+            if (interval[1] > start && end > interval[0]) {
+                return false;
+            }
+        }
+        int[] interval = new int[] {start, end};
+        this.Calender.add(interval);
+        this.Calender.sort((a, b) -> Integer.compare(a[0], b[0]));
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
