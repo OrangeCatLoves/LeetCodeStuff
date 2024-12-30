bfb621270273ea4f6ff5072bddd2f7639678d002
Low Wei Bin <e1121707@u.nus.edu>
2024-12-30 18:26:14 +0800

    Create DuplicateEmails.sql

---
+++ DuplicateEmails.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+/*
+The key is the GROUP BY operation. After the GROUP BY clause groups the emails, there is only one row per unique email in the result set. The 
+HAVING clause then ensures that only emails that appear more than once are included.
+Since the email is already grouped (and DISTINCT isn't needed), each duplicate email is printed only once, because the GROUP BY eliminatesmultiple # occurrences of the same email in the result.
+*/
+
+SELECT email
+FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1;
