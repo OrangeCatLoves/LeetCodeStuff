47624d1e2c3676a7aead668d6ba2610e5b244093
Low Wei Bin <e1121707@u.nus.edu>
2025-01-10 18:30:45 +0800

    Update ReducingDishes.py

--- ReducingDishes.py
+++ ReducingDishes.py
@@ -1,14 +1,29 @@
 # satisfaction[i] (Subarray) = satisfaction[i - 1] (Subarray) + sum(satisfaction[i])
+# When considering the subarrays when it is sorted in descending order
 # To maximise the value added, ensure that the sum is always > 0
 
+# class Solution:
+#     def maxSatisfaction(self, satisfaction: List[int]) -> int:
+#         satisfaction.sort(reverse = True)
+#         _sum, result = 0, 0
+#         for satis in satisfaction:
+#             _sum += satis
+#             if (_sum < 0):
+#                 continue
+#             result += _sum
+#         return result
+
+# 0/1 Knapsack problem, either you consider the dish or you don't
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
-        satisfaction.sort(reverse = True)
-        _sum, result = 0, 0
-        for satis in satisfaction:
-            _sum += satis
-            if (_sum < 0):
-                continue
-            result += _sum
-        
-        return result
+        satisfaction = sorted(satisfaction)
+        @cache
+        def dfs(i, c):
+            if i >= len(satisfaction):
+                return 0
+            # Consider the max of either you include the dish or you don't
+            res = max(satisfaction[i] * c + dfs(i + 1, c + 1), dfs(i + 1, c))
+            return res
+        return dfs(0, 1) 
+
+# Both solution works, but the commented solution is so unintuitive
